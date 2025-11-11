d24b76a1053ad8140cc91db9c35092b0605c21e9
P27389 <andreas.mueller@fh-hagenberg.at>
2025-11-11 15:01:25 +0100

    ue06

---
+++ ue06/step3/clock-project/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import Anzeige from "@/components/Anzeige.vue";
+
+const now = ref(new Date().toLocaleTimeString());
+
+setInterval(() => {
+  now.value = new Date().toLocaleTimeString();
+}, 1000);
+</script>
+
+<template>
+  Uhrzeit: {{ now }}
+
+  <h1>Anzeige Komponente</h1>
+  <h2>1</h2>
+  <Anzeige></Anzeige>
+  <h2>2</h2>
+  <Anzeige></Anzeige>
+</template>
+
+<style scoped>
+
+</style>
---
+++ ue06/step3/clock-project/src/components/Anzeige.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import {ref, type Ref} from "vue";
+
+const view: Ref<boolean | undefined> = ref(false);
+
+setInterval(() => {
+  // Variante 1: im Kreis
+  if (view.value === true) {
+    view.value = false;
+  } else if (view.value === false) {
+    view.value = undefined;
+  } else {
+    view.value = true;
+  }
+
+  // Variante 2: Zufall
+  const r = Math.floor(Math.random() * 3);
+  view.value = r === 0 ? true : r === 1 ? false : undefined;
+}, 2000);
+</script>
+
+<template>
+  <div v-if="view === true">
+    View ist true :)
+  </div>
+  <div v-else-if="view === false">
+    View ist false :(
+  </div>
+  <p v-else>
+    View ist undefined :-o
+  </p>
+</template>
+
+<style scoped>
+
+</style>
